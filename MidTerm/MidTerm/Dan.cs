ab63b1e0ae44ce77ce37af055df1df72ae96d3b9
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2019-04-14 12:45:50 +0700

    new

---
+++ MidTerm/MidTerm/Dan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidTerm
+{
+    class Dan
+    {
+        public PointF location;
+        public PointF locationOld;
+        public bool shoot = false;
+        public int type;
+    }
+}
